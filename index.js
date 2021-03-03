0d3dddd7147b462bda17f4349f3ee2b40c12af38
Sandeep Suman <54541515+SandeepKrSuman@users.noreply.github.com>
2021-03-04 00:58:13 +0530

    initial commits

---
+++ index.js
@@ -0,0 +1,28 @@
+const apiCall = async () => {
+  const response = await fetch('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY');
+  const apiData = await response.json();
+
+  if(response.status === 200){
+    const dt = apiData.date;
+    const title = apiData.title;
+    const cr = apiData.copyright;
+    const expl = apiData.explanation;
+    const imgurl = apiData.hdurl;
+
+    document.getElementById("dated").innerHTML = dt;
+    document.getElementById("dated").style.border = "1px dotted black;";
+    document.getElementById("title").innerHTML = "\""+title+"\"";
+    document.getElementById("picod").src = imgurl;
+    document.getElementById("copyright").innerHTML = "copyright: "+cr;
+    document.getElementById("explaination").innerHTML = expl;
+  }
+  else{
+    document.getElementById("title").innerHTML = ":( Error: " + response.statusText;
+    document.getElementById("title").style.color = "red";
+  }
+
+}
+
+window.onload = function() {
+  apiCall();
+};
